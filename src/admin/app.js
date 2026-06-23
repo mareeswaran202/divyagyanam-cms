1bd00fc51f88c0b54ae9bdad222cd480574e1a89
mareeswaran.g@gmail.com <mareeswaran.g@gmail.com>
2026-06-23 21:18:33 +0530

    home page ui

---
+++ src/admin/app.js
@@ -0,0 +1,21 @@
+ 
+export default {
+  config: {
+      
+    auth: {
+      logo: "/uploads/divyagyanam_logo_1cf3bd12c0.png",
+    },
+
+    menu: {
+      logo: "/uploads/divyagyanam_logo_1cf3bd12c0.png",
+    },
+
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "Welcome DivyaGyanam",
+        "Auth.form.welcome.subtitle":
+          "Log in to your CMS account",
+      },
+    },
+  },
+};
